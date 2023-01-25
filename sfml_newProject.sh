81a520bb5ad3b04b5f18a90c89e0a72f66b3d194
asiangoldfish <duongkhai@gmail.com>
2023-01-25 23:16:35 +0100

    Initial commit

---
+++ sfml_newProject.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+#
+# Generate new SFML project template for Visual Studio 2017
+
+SFML_PROJECT="$(dirname "$0")/sfml_template.tar.gz"
+TARGET=""
+
+# Check whether target destination was passed
+if [ -z "$1" ]; then
+	echo "Missing target destination"
+	exit 1
+else
+	TARGET="$1"
+fi
+
+echo "$TARGET"
+
+# Attempt to find the SFML project
+if [ ! -f "$SFML_PROJECT" ]; then
+	echo "Could not find the SFML template project"
+	exit 1
+fi
+
+echo "Generating new SFML project..."
+
+mkdir -p "$TARGET" || { echo "Could not create new project. Missing permission?"; exit 1; }
+
+tar -xvf "$SFML_PROJECT" -C "$TARGET" || { echo "Could not create new project. Missing permission?"; exit 1; }
+
+echo "Successfully created new SFML project at $TARGET"
